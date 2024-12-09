76ce9a74b9cca3069ed881ee5b751263ed72ab80
Jenny Urtubey <jemaer20@gmail.com>
2024-12-08 22:40:35 -0500

    Fin sección 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { Observable } from 'rxjs/internal/Observable';
+import { Componente } from './interfaces/interfaces';
+import { DataService } from './services/data.service';
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+selector: 'app-root',
+templateUrl: 'app.component.html',
+styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  componente: Observable<Componente[]>;
+
+  ngOnInit() {
+    this.componente = this.dataService.getMenuOpts();
+
+  }
+
+  constructor(private dataService: DataService) {
+
+  }
+
+  initializeApp() {
+    this.componente = this.dataService.getMenuOpts();
+  }
+
 }
--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,31 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { Observable } from 'rxjs';
 import { ComponentsModule } from '../../components/components.module';
-
+import { DataService } from '../../services/data.service';
 interface Componente {
-icon: string;
-name: string;
-redirectTo: string;
+  icon: string;
+  name: string;
+  redirectTo: string;
 }
 @Component({
-selector: 'app-inicio',
-templateUrl: './inicio.page.html',
-styleUrls: ['./inicio.page.scss'],
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
 
-componentes: Componente[] = [
-{
-icon: 'american-football-outline',
-name: 'Action Sheet',
-redirectTo: '/action-sheet'
-},
-{
-icon: 'alert-circle-outline',
-name: 'Alert',
-redirectTo: '/alert'
-},
-];
-constructor() { }
-ngOnInit() {
-}
+  componentes: Observable<Componente[]>;
+
+  constructor(private menuCtrl: MenuController,
+    private dataService: DataService) { }
+
+  ngOnInit() {
+    this.componentes = this.dataService.getMenuOpts();
+  }
+  mostrarMenu() {
+    this.menuCtrl.open('first');
+  }
+
 }
